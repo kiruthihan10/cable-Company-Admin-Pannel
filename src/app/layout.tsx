3fe578546d92b04298858775ce261649482c5882
kiruthihan nagarajan <kiruthihan10@gmail.com>
2024-03-10 12:43:38 +0530

    Login UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ConfigProvider } from "antd";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ConfigProvider
+        theme={{
+          token: {},
+          components: {
+            Card: {
+              headerBg : '#b5f5ec'
+            }
+          }
+        }}
+      >
+        <body className={inter.className}>{children}</body>
+      </ConfigProvider>
     </html>
   );
 }
