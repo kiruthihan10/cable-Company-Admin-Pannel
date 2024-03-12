0efd3c1bd11b403339a58d6a8c05d687b752a444
kiruthihan nagarajan <kiruthihan10@gmail.com>
2024-03-12 11:27:25 +0530

    companies Services

--- src/external/service.ts
+++ src/external/service.ts
@@ -1,5 +1,6 @@
 import { userType } from "@/interfaces/user";
-import axios from "axios";
+import { useUserStore } from "@/stores/userStore";
+import axios, { AxiosError } from "axios";
 
 export interface ILoginRequest {
   username: string;
@@ -9,6 +10,22 @@ export interface ILoginRequest {
 interface ILoginResponse {
   token: string;
   userType: userType;
+}
+
+interface ISingleCompanyResponse {
+  id: number;
+  name: string;
+  contactNumber: number;
+  contactPerson: string;
+  totalCustomers: number;
+  totalEmployees: number;
+  totalPayments: number;
+  totalAreas: number;
+}
+
+interface ICompaniesResponse {
+  companies: ISingleCompanyResponse[];
+  noOfCompanies: number;
 }
 
 const baseURL =
@@ -22,4 +39,39 @@ export const login = (req: ILoginRequest) => {
     username: username,
     password: password,
   });
+};
+
+export const useAPIController = () => {
+  const user = useUserStore((state) => state.user);
+  const setToken = useUserStore((state) => state.setToken);
+  axios.defaults.headers["Authorization"] = "Bearer " + user?.token;
+  const get = <T>(url: string, params?: unknown) => {
+    return axios
+      .get<T>(baseURL + url, { params: params })
+      .catch(async (error: Error | AxiosError) => {
+        handleUnAuth(error);
+      });
+  };
+  const post = <T>(url: string, data?: any) => {
+    return axios
+      .post<T>(baseURL + url, data)
+      .catch(async (error: Error | AxiosError) => {
+        handleUnAuth(error);
+      });
+  };
+  const handleUnAuth = async (error: Error | AxiosError) => {
+    if (axios.isAxiosError(error)) {
+      if (error.status == 401) {
+        if (user) {
+          setToken((await login(user)).data.token);
+        }
+      }
+    }
+  };
+  const getCompanies = (page?: number, size?: number) => {
+    return get<ICompaniesResponse>("/Company", {
+      page,
+      size,
+    });
+  };
 };
