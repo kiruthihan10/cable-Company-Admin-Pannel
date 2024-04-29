30acbaea8d80623e05d3578435b02780e9703aa1
kiruthihan nagarajan <kiruthihan10@gmail.com>
2024-04-29 21:54:24 +0530

    Settings Page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import FormButton from "@/components/unitComponents/formComponents/button";
+import { urls } from "@/constants";
+import { mutationKeys, queryKeys } from "@/external/keys";
+import { useAPIController } from "@/external/service";
+import { useWindow } from "@/external/utils";
+import {
+  IUpdateEmployeeForm,
+  UpdateEmployeeFormInitialValues,
+  UpdateEmployeeFormValidation,
+} from "@/forms/employee/employeeForm";
+import UpdateEmployeeForm from "@/forms/employee/updateEmployeeForm";
+import AddUserForm from "@/forms/user/addUserForm";
+import { useUserStore } from "@/stores/userStore";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Flex, Card } from "antd";
+import { Form, Formik } from "formik";
+import { useRouter } from "next/navigation";
+
+const Settings = () => {
+  const router = useRouter();
+  const { windowWidth } = useWindow();
+  const { getEmployee, updateEmployee } = useAPIController();
+  const user = useUserStore((state) => state.user);
+  const { data, isLoading, error, refetch } = useQuery({
+    queryFn: ({ queryKey }) => {
+      return getEmployee(queryKey[1]);
+    },
+    queryKey: [queryKeys.employees, user?.username || ""],
+  });
+  const { mutate } = useMutation({
+    mutationKey: [mutationKeys.employee],
+    mutationFn: updateEmployee,
+    onSuccess: () => {
+      refetch();
+    },
+  });
+  const initialValues: IUpdateEmployeeForm = {
+    ...UpdateEmployeeFormInitialValues,
+    ...data?.data,
+  };
+  const onSubmit = (values: IUpdateEmployeeForm) => {
+    if (values.phoneNumber != undefined) {
+      mutate({
+        id: user?.username || "",
+        ...values,
+        phoneNumber: values.phoneNumber,
+      });
+    }
+  };
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={UpdateEmployeeFormValidation}
+      enableReinitialize
+      onSubmit={onSubmit}
+    >
+      <Flex
+        justify={"center"}
+        style={{
+          width: `${windowWidth * 0.8}px`,
+        }}
+      >
+        <Form>
+          <Card
+            title={`Update ${data?.data.firstName} ${data?.data.lastName}`}
+            style={{ width: `${windowWidth * 0.64}px` }}
+          >
+            <Flex vertical gap="small" style={{ width: "100%" }}>
+                <AddUserForm />
+              <FormButton center text="Update" isSubmit />
+            </Flex>
+          </Card>
+        </Form>
+      </Flex>
+    </Formik>
+  );
+};
+
+export default Settings;
