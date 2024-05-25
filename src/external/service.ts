00f1e2f1ebbe7d811f943ed47eb75feffda08072
kiruthihan nagarajan <kiruthihan10@gmail.com>
2024-05-25 13:45:01 +0530

    API URL Update

--- src/external/service.ts
+++ src/external/service.ts
@@ -39,7 +39,7 @@ interface ILoginResponse {
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://cablecompanies.azurewebsites.net/"
+    ? "https://cablecompanies.azurewebsites.net"
     : "http://127.0.0.1:5054";
 
 export const login = (req: ILoginRequest) => {
