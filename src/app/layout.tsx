bf9c6259d3cfa92c9efd347964774e98508e29c4
kiruthihan nagarajan <kiruthihan10@gmail.com>
2024-06-09 12:05:08 +0530

    Installable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon2.png" type="image/png" sizes="512x512" />
+      </head>
       <StylingConfig>
         <Providers>
           <Header />
